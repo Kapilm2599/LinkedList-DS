d167afbff9bad3432fa4e4b116a5105527a553c9
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-24 18:53:11 +0530

    Added: Ability to create linked list by adding element

--- src/com/linkedlist/ds/LinkedList.java
+++ src/com/linkedlist/ds/LinkedList.java
@@ -15,19 +15,34 @@ public class LinkedList {
 
 	}
 
-	public void CreateList(Object data) {
+	public void addFirst(Object data) {
 
 		Node newNode = new Node(data); 
-		System.out.println(newNode.data + " ");
+		newNode.next = head;
+		head = newNode;
+	}
+	
+	public void display() {
+		Node temp = head;
+		while(temp != null) {
+			if (temp.next != null)
+				System.out.print(temp.data + " -> ");
+			else
+				System.out.println(temp.data);
+			temp = temp.next;
+		}
 	}
 
 	public static void main(String[] args) {
 
 		LinkedList list = new LinkedList();
 		System.out.println("welcome to LinkedList Problem");
-		list.CreateList(56);
-		list.CreateList(30);
-		list.CreateList(70);
+		list.addFirst(70);
+		list.display();
+		list.addFirst(30);
+		list.display();
+		list.addFirst(56);
+		list.display();
 
 	}
 
