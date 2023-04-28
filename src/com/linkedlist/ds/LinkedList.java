9c874f9d19eab3e7c2fe296526b74ad6325aaa39
Kapilm2599 <mahajankapil575@gmail.com>
2023-04-28 13:47:19 +0530

    Added: Ability to add in last

--- src/com/linkedlist/ds/LinkedList.java
+++ src/com/linkedlist/ds/LinkedList.java
@@ -22,6 +22,21 @@ public class LinkedList {
 		head = newNode;
 	}
 	
+	public void addLast(Object data) {
+		Node newNode = new Node(data);
+		Node temp = head;
+		if (head == null) {
+			head = newNode;
+		}
+
+		else {
+			while (temp.next != null) {
+				temp = temp.next;
+			}
+			temp.next = newNode;
+		}
+	}
+	
 	public void display() {
 		Node temp = head;
 		while(temp != null) {
@@ -37,13 +52,21 @@ public class LinkedList {
 
 		LinkedList list = new LinkedList();
 		System.out.println("welcome to LinkedList Problem");
+		System.out.println("Adding Nodes At First");
 		list.addFirst(70);
 		list.display();
 		list.addFirst(30);
 		list.display();
 		list.addFirst(56);
 		list.display();
-
+		System.out.println();
+		System.out.println("Adding Nodes At last");
+		list.addLast(56);
+		list.display();
+		list.addLast(30);
+		list.display();
+		list.addLast(70);
+		list.display();
 	}
 
 }
